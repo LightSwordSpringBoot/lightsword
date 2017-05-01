04026eb923ccc649f644f77afe6b52a5d6c80432
jack <guangjian.cgj@alibaba-inc.com>
2017-05-01 23:31:58 +0800

    logback-dev.groovy

--- src/main/resources/logback-dev.groovy
+++ src/main/resources/logback-dev.groovy
@@ -1,35 +1,49 @@
-//
-// Built on Sun Apr 30 19:39:06 UTC 2017 by logback-translator
-// For more information on configuration files in Groovy
-// please see http://logback.qos.ch/manual/groovy.html
-
-// For assistance related to this tool or configuration files
-// in general, please contact the logback user mailing list at
-//    http://qos.ch/mailman/listinfo/logback-user
-
-// For professional support please see
-// http://www.qos.ch/shop/products/professionalSupport
-
 //https://logback.qos.ch/translator/asGroovy.html
 
-
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.filter.ThresholdFilter
+import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.rolling.RollingFileAppender
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 
-import static ch.qos.logback.classic.Level.*
+import java.nio.charset.Charset
 
+import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.classic.Level.TRACE
+
+def USER_HOME = System.getProperty("user.home")
 def APP_NAME = "lightsword"
+def LOG_PATTERN = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{35} - %msg %n"
+def LOG_FILE = "${USER_HOME}/logs/${APP_NAME}"
+def FILE_NAME_PATTERN = "${APP_NAME}.%d{yyyy-MM-dd}.log"
+
+scan("60 seconds")
+
+context.name = "${APP_NAME}"
+jmxConfigurator()
+
 logger("org.springframework.web", INFO)
 logger("com.springboot.in.action", TRACE)
-appender("dailyRollingFileAppender", RollingFileAppender) {
-    file = "${user.home}/logs/${APP_NAME}"
-    rollingPolicy(TimeBasedRollingPolicy) {
-        fileNamePattern = "${APP_NAME}.%d{yyyy-MM-dd}.log"
-        maxHistory = 30
-    }
+logger("org.apache.velocity.runtime.log", INFO)
+
+appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{35} - %msg %n"
+        pattern = "${LOG_PATTERN}"
+        charset = Charset.forName("utf8")
     }
 }
-root(DEBUG, ["CONSOLE", "dailyRollingFileAppender"])
+
+appender("dailyRollingFileAppender", RollingFileAppender) {
+    file = "${LOG_FILE}"
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "${FILE_NAME_PATTERN}"
+        maxHistory = 30
+    }
+    filter(ThresholdFilter) {
+        level = TRACE
+    }
+    encoder(PatternLayoutEncoder) {
+        pattern = "${LOG_PATTERN}"
+    }
+}
+root(TRACE, ["CONSOLE", "dailyRollingFileAppender"])
